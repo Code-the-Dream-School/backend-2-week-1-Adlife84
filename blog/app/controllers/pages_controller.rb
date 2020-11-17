a38dbddeb121ad91b08c044500202197136b72fe
Andrei Komolov <andreykomolov@gmail.com>
2020-11-17 10:06:51 -0500

    Add show.html.erb view template for Page

--- blog/app/controllers/pages_controller.rb
+++ blog/app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
     def index
         @pages = Page.all
     end
+
+    def show
+        @page = Page.find(params[:id])
+        # render plain: @page.title //if you don't use render in the block Rails lookink template in view/pages.show.html.erb
+    end
 end
